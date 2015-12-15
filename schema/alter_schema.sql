78a8722963178f6a0242512b9453f9d20aa3bc02
Pierre Legay <Pierre.Legay@recia.fr>
2015-12-15 10:22:50 +0100

    reorders the database changes

--- schema/alter_schema.sql
+++ schema/alter_schema.sql
@@ -5,10 +5,6 @@ alter table prepared_request
 ADD COLUMN data_source varchar(100) not null,
 Add COLUMN ldap_suffix varchar(100) not null;
 
--- on complete les donnée déjà existantes.
-update prepared_request set data_source = 'ldap_request', ldap_suffix='ou=people,dc=esco-centre,dc=fr';
-
-
 -- modification de la vue ?
 
 create or replace VIEW v_model_editors 
@@ -23,3 +19,8 @@ FROM model, prepared_request,j_model_request
 WHERE j_model_request.id_request=prepared_request.id_request 
 AND model.id=j_model_request.id_model 
 ORDER BY modelname;
+
+
+-- on complete les donnée déjà existantes.
+update prepared_request set data_source = 'ldap_request', ldap_suffix='ou=people,dc=esco-centre,dc=fr';
+
